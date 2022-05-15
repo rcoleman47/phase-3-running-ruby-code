d270c62644d6e9c2464e9c82cf700ea89036900b
ryanadams <rcoleman.adams@gmail.com>
2022-05-15 18:36:13 -0500

    commit tree 88769fb52137ca8c27f60b732501355be30fae5d

---
+++ app.rb
@@ -0,0 +1,9 @@
+# app.rb
+puts [1, 2, 3]
+p [1, 2, 3]
+
+pp [{ id: 1, hello: "world"}, {id: 2, hello: "Ruby"}, {id: 3, hello: "moon"}, {id: 4, hello: "learner"}]
+
+puts "Hello World!"
+
+print "Pass this test, please."
